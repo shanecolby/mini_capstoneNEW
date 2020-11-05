78aabcdb97337bc73f6e3aa3222aa935f78992e6
shane <shanecolbyzero8@gmail.com>
2020-11-05 08:21:18 -0600

    add all products method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,11 @@
 class Api::ProductsController < ApplicationController
   def first
+    @product = Product.last
     render "first.json.jb"
+  end
+
+  def total
+    @products = Product.all
+    render "all.json.jb"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/first_product" => "products#first"
+    get "/all_products" => "products#total"
   end
 end
