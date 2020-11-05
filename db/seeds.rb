3428b4facd73900d8158393c7ff945a63c524160
shane <shanecolbyzero8@gmail.com>
2020-11-05 07:17:01 -0600

    added products in both console and seeds folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+product = Product.new(name: "Keyboard", price: 75.00, img_url: "keyboard.computer.tkl.amazon", description: "Mechanical Keyboard wired")
+product.save
+
+product = Product.new(name: "Computer mouse", price: 60.00 , img_url: "computer.mouse", description: "wired computer mouse")
+product.save
+
+product = Product.new(name: "Computer desk", price: 200.00, img_url: "desks.computer.wood.l-shaped.com", description: "L-shaped computer desk")
+product.save
+
+product = Product.new(name: "Computer chair", price: 150.00, img_url: "desk.chair.computer.leather", description: "Adjustable computer chair with lumbar support")
+product.save
+
+product = Product.new(name: "desk lamp", price: 15.00, img_url: "desk.accessory.lamp", description: "10 inch desk lamp")
+product.save
