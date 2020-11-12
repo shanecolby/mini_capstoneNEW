cf1df35f26a3015543aeaa94d2cd38f4f6003429
shane <shanecolbyzero8@gmail.com>
2020-11-11 21:22:25 -0600

    added migration to change price from integer to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_031027) do
+ActiveRecord::Schema.define(version: 2020_11_12_032126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price"
     t.string "img_url"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
