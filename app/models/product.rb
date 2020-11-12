5ccc0ef5b23d6432479914f1199482bdd3d2bdec
shane <shanecolbyzero8@gmail.com>
2020-11-11 18:24:41 -0600

    added model methods for tax, total price and if the item is discounted

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,21 @@
 class Product < ApplicationRecord
-  def price_tax
+  def tax
     tax = price * (0.09)
+    # total = price + tax
+  end
+
+  def is_discounted?
+    if price < 10
+      "true"
+    else
+      false
+    end
+  end
+
+  def total
     total = price + tax
   end
+
+
   
 end
