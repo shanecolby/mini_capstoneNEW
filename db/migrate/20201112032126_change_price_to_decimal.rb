7873efbee87ba49fb844369bdf5ff31d24a995be
shane <shanecolbyzero8@gmail.com>
2020-11-12 18:16:39 -0600

    added percision and scale to price migration

--- db/migrate/20201112032126_change_price_to_decimal.rb
+++ db/migrate/20201112032126_change_price_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[6.0]
   def change
-    change_column :products, :price, :decimal
+    change_column :products, :price, :decimal, percision: 8, scale: 2
   end
 end
