3248eb577cca0f929a36f83ecb31d4b1ad2de695
shane <shanecolbyzero8@gmail.com>
2020-11-17 21:16:25 -0600

    added sign up/create action

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
     delete "/products/:id" => "products#destroy"
+
+    post "/users" => "users#create"
   end
 end
