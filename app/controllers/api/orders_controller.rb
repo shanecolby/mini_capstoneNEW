4459c82007a34d9e5a74a7b298830c51f9bc5f51
shane <shanecolbyzero8@gmail.com>
2020-11-19 21:03:04 -0600

    refactored code in controller

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,14 +1,24 @@
 
 class Api::OrdersController < ApplicationController
   def create
-    if current_user
-      @order = Order.new(
-      product_id: params[:product_id],
-      quantity: params[:quantity]
-      )
-    end
+    product = Product.find_by(id: params[:product_id]
     
+    calculated_subtotal = params[:quantity].to_i * product.price
+    tax_rate = 0.09
+    calculated_tax = params[:price] 
+    calculated_total = 
+
+
+    @order = Order.new(
+      user_id: current_user.id, 
+      product_id: params[:product_id], 
+      quantity: params[:quantity], 
+      subtotal: 20, 
+      tax:1.80, 
+      total: 21.80
+     )
    
+    @order.save
     render "show.json.jb"
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,16 +2,6 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  def sub_total
-    price
-  end
 
-  def tax
-    price * 0.09
-  end
-
-  def total
-    price + tax
-  end
 
 end
