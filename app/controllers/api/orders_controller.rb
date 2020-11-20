ba0c417719cb58530255a0c63653a51801983e50
shane <shanecolbyzero8@gmail.com>
2020-11-19 19:54:48 -0600

    created orders controller and added views show.json

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,2 +1,14 @@
+
 class Api::OrdersController < ApplicationController
+  def create
+    if current_user
+      @order = Order.new(
+      product_id: params[:product_id],
+      quantity: params[:quantity]
+      )
+    end
+    
+   
+    render "show.json.jb"
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,17 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  def sub_total
+    price
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
 end
