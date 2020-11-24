cc7e263c7abc1549b7d9a427358edb5405e1c7cd
shane <shanecolbyzero8@gmail.com>
2020-11-23 18:47:58 -0600

    add category model add categoryproduct model, create instances for both, write has_many/belongs_to relationships between category,product, changeproduct

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -8,22 +8,18 @@ class Api::OrdersController < ApplicationController
   end
   
   
-  
-  
-  
-  
   def create
     product = Product.find_by(id: params[:product_id]
     
     calculated_subtotal = params[:quantity].to_i * product.price
     tax_rate = 0.09
-    calculated_tax = params[:price] 
-    calculated_total = 
+    calculated_tax = calculated_subtotal * tax_rate 
+    calculated_total = calculated_subtotal + calculated_tax
 
 
     @order = Order.new(
       user_id: current_user.id, 
-      product_id: params[:product_id], 
+      product_id: params[:product_id],
       quantity: params[:quantity], 
       subtotal: calculated_subtotal, 
       tax: calculated_tax, 
---
+++ app/models/category.rb
@@ -0,0 +1,3 @@
+class Category < ApplicationRecord
+  has_many :Category_products
+end
